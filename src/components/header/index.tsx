7fb90068f94d2a8fe353513cdf7c063b31659801
johnatan sousa paixao <johnatan.sousa@gmail.com>
2023-05-09 21:09:03 -0300

    header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,28 +1,27 @@
-import Link from "next/link";
+import Link from "next/link"
 import styles from "./header.module.sass"
-import { HiCurrencyDollar } from 'react-icons/hi'
+import { AiOutlineMenu } from 'react-icons/ai'
+import { RiFilePaper2Line } from 'react-icons/ri'
+import { BsPersonWorkspace } from 'react-icons/bs'
 
 export function Header() {
   return (
     <header className={styles.header}>
       <section className={styles.content}>
-        <div className={styles.Menu}>
-          <select>
-            <option value="fruit">Fruit</option>
-            <option value="vegetable">Vegetable</option>
-            <option value="meat">Meat</option>
-          </select>
-        </div>
+      <button className={styles.btn_Registrar}>Registrar</button>
+
         <nav className={styles.nav}>
           <div>
             <Link href="/dashboard">
-              <span>F</span>ull <span>W</span>allet
+              <span>R</span>egistrar <span>T</span>oner
             </Link>
-            <HiCurrencyDollar />
+            <BsPersonWorkspace />
           </div>
         </nav>
 
-        <button className={styles.button}>Acessar</button>
+        <button className={styles.btn_Acessar}>Acessar</button>
+
+        <AiOutlineMenu className={styles.menu_mobile}/>
       </section>
     </header>
   )
