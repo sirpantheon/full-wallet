3787558bfc920244d0c72c6e2fd9f005813a4f36
johnatan sousa paixao <johnatan.sousa@gmail.com>
2023-07-17 22:33:05 -0300

    teste

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,11 @@ import { Roboto } from 'next/font/google'
 import Head from 'next/head'
 import styles from '../styles/home.module.scss'
 import { BsWallet2 } from 'react-icons/bs';
+import { GetStaticProps } from 'next';
+import { addDoc, collection, query, orderBy, where, onSnapshot, doc, deleteDoc, getDocs } from 'firebase/firestore'
+import { DB } from '@/services/firebaseConnection';
+import { useEffect, useState } from 'react';
+
 
 const roboto = Roboto({
   weight: ['400', '700'],
@@ -10,7 +15,66 @@ const roboto = Roboto({
   variable: '--font-roboto',
 })
 
-export default function Home() {
+interface IHomeProps {
+  id: string
+  created: string
+  impressora: string
+  observacao?: string
+  ping: string
+  done: boolean
+  serie: string
+  setor: string
+  toner: string
+  user: string
+}
+
+export default function Home({ toner }: IHomeProps) {
+
+  const [w1330x, SetW1330x] = useState(0)
+  const [registros, setRegistros] = useState<IHomeProps[]>([])
+
+  const QuantTonner = collection(DB, "toners")
+  const toners = query(QuantTonner, where("toner", "==", "w1330x"));
+
+  console.log(QuantTonner)
+
+  useEffect(() => {
+    async function loadRegister() {
+      const registerRef = collection(DB, "w1330x")
+      const q = query(
+        registerRef,
+        orderBy("created", "desc"),
+        where("toner", "==", toner)
+      )
+      onSnapshot(q, (snapshot) => {
+        let lista = [] as IHomeProps[]
+
+        console.log(toner)
+        snapshot.forEach((doc) => {
+          lista.push({
+            id: doc.id,
+            created: doc.data().created,
+            impressora: doc.data().impressora,
+            observacao: doc.data().observacao,
+            ping: doc.data().ping,
+            done: doc.data().done,
+            serie: doc.data().serie,
+            setor: doc.data().setor,
+            toner: doc.data().toner,
+            user: doc.data().user,
+          })
+
+        })
+        setRegistros(lista)
+
+      })
+
+    }
+    loadRegister()
+  }, [toner])
+
+  
+
   return (
     <div className={styles.container}>
       <Head>
@@ -23,11 +87,29 @@ export default function Home() {
             Esse sistema foi feito para voce organizar e ter controle de quantos insumos voce usar ao longo do més... registre tudo.
           </p>
           <div className={styles.infoContent}>
-            <span>+12 Registros</span>
-            <span>+90 Comentarios</span>
+            <span>{} Registros</span>
+            <span>{} w1330x</span>
           </div>
         </section>
       </main>
     </div>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+
+  const RegisterTonner = collection(DB, "registertonner")
+  const Toners = collection(DB, "toners")
+
+  const RegisterSnapshot = await getDocs(RegisterTonner)
+  const TonersSnapshot = await getDocs(Toners)
+
+  
+  return {
+    props: {
+      registertonner: RegisterSnapshot.size || 0,
+      Toners: TonersSnapshot.size || 0,
+    },
+    revalidate: 60,
+  }
 }
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,6 @@
 export function FormatDate(date: string) {
-  const day = new Date(date).getDate() + 1 < 9 ? `0${new Date(date).getDate() + 1}` : new Date(date).getDate() + 1
-  const month = new Date(date).getMonth() + 1 < 9 ? `0${new Date(date).getMonth() + 1}` : new Date(date).getMonth() + 1
+  const day = new Date(date).getDate() + 1 <= 9 ? `0${new Date(date).getDate() + 1}` : new Date(date).getDate() + 1
+  const month = new Date(date).getMonth() + 1 <= 9 ? `0${new Date(date).getMonth() + 1}` : new Date(date).getMonth() + 1
   const years = new Date(date).getFullYear()
   const FullDate = day + "/" + month + "/" + years
 
